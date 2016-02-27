cac322dfe3c8ba6273574353adc76da560cf40e8
DimaDen94 <DimaDen94@MAIL.RU>
2016-02-27 17:07:32 +0200

    level29.lesson09.task01 completed

---
+++ src/com/javarush/test/level29/lesson09/task01/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level29.lesson09.task01;
+
+import java.math.BigDecimal;
+
+/* Этот странный BigDecimal
+Исправьте ошибку реализации, приводящую к погрешности вычисления, в методе getValue.
+Сигнатуру метода не менять. Округле0ние не использовать.
+Читайте доп. статью про особенности автобоксинга.
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getValue(1.1d, 1.2d));
+    }
+
+    public static BigDecimal getValue(double v1, double v2) {
+        BigDecimal decimal = BigDecimal.valueOf(v1).add(BigDecimal.valueOf(v2));
+        return decimal;
+    }
+}
