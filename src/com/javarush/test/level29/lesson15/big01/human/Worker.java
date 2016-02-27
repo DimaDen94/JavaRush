24fdc1820f5d43493d0c41b5ea2b0498c57335b4
DimaDen94 <DimaDen94@MAIL.RU>
2016-02-27 19:05:50 +0200

    level29.lesson15.big01. task 1 completed

---
+++ src/com/javarush/test/level29/lesson15/big01/human/Worker.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level29.lesson15.big01.human;
+
+public class Worker {
+    private Human human;
+    private double salary;
+    public String company;
+
+    public Worker(String name, int age)
+    {
+        human = new Human(false);
+    }
+
+    public void live() {
+        human.live();
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSlr(double salary) {
+        this.salary = salary;
+    }
+}
