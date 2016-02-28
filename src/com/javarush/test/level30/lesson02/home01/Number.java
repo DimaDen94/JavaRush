73b6b3855e505ed4fa627d2d9409a668743e33a5
DimaDen94 <DimaDen94@MAIL.RU>
2016-02-28 22:02:50 +0200

    level30.lesson02.home01  completed

---
+++ src/com/javarush/test/level30/lesson02/home01/Number.java
@@ -0,0 +1,37 @@
+package com.javarush.test.level30.lesson02.home01;
+
+public class Number {
+    private NumerationSystem numerationSystem;
+    private String digit;
+
+    public Number(NumerationSystem numerationSystem, String digit) {
+        this.numerationSystem = numerationSystem;
+        this.digit = digit;
+    }
+
+    public void setNumerationSystem(NumerationSystem numerationSystem)
+    {
+        this.numerationSystem = numerationSystem;
+    }
+
+    public void setDigit(String digit)
+    {
+        this.digit = digit;
+    }
+
+    public NumerationSystem getNumerationSystem() {
+        return numerationSystem;
+    }
+
+    public String getDigit() {
+        return digit;
+    }
+
+    @Override
+    public String toString() {
+        return "Number{" +
+                "numerationSystem=" + numerationSystem +
+                ", digit='" + digit + '\'' +
+                '}';
+    }
+}
