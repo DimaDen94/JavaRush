4165140384f81063e007be1be7a6a52a51a37d82
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-01 01:23:59 +0200

    level30.lesson15.big01. Task №3  completed. so easy

--- src/com/javarush/test/level30/lesson15/big01/MessageType.java
+++ src/com/javarush/test/level30/lesson15/big01/MessageType.java
@@ -5,4 +5,10 @@ package com.javarush.test.level30.lesson15.big01;
  */
 public enum MessageType
 {
+    NAME_REQUEST,
+    USER_NAME,
+    NAME_ACCEPTED,
+    TEXT,
+    USER_ADDED,
+    USER_REMOVED
 }
