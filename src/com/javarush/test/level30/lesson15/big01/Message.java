83c273d473b377d1dfc89b07726e408e08837a16
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-01 01:36:37 +0200

    level30.lesson15.big01. Task №3  completed. so easy

--- src/com/javarush/test/level30/lesson15/big01/Message.java
+++ src/com/javarush/test/level30/lesson15/big01/Message.java
@@ -1,8 +1,34 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.Serializable;
+
 /**
  * Created by Dmitry on 29.02.2016.
  */
-public class Message
+public class Message implements Serializable
 {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type)
+    {
+        this.type = type;
+        this.data = null;
+    }
+
+    public Message(MessageType type, String data)
+    {
+        this.type = type;
+        this.data = data;
+    }
+
+    public MessageType getType()
+    {
+        return type;
+    }
+
+    public String getData()
+    {
+        return data;
+    }
 }
