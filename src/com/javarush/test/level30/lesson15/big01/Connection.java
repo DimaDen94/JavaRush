6c9f5f524914971f1f6ca9f1fc6a93ef0a381d5b
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-02 14:44:41 +0200

    level30.lesson15.big01. Task №5  completed.

--- src/com/javarush/test/level30/lesson15/big01/Connection.java
+++ src/com/javarush/test/level30/lesson15/big01/Connection.java
@@ -1,8 +1,67 @@
 package com.javarush.test.level30.lesson15.big01;
 
-/**
- * Created by Dmitry on 29.02.2016.
- */
-public class Connection
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Connection implements Closeable
 {
+
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException
+    {
+        this.socket = socket;
+        this.out = new ObjectOutputStream(socket.getOutputStream());
+        this.in = new ObjectInputStream(socket.getInputStream());
+    }
+
+
+    public void send(Message message) throws IOException
+    {
+
+        synchronized (out)
+        {
+            out.writeObject(message);
+            out.flush();
+
+        }
+
+    }
+
+
+    public Message receive() throws IOException, ClassNotFoundException
+    {
+
+        Message message;
+
+        synchronized (in)
+        {
+            message = (Message) in.readObject();
+            return message;
+        }
+
+    }
+
+
+    public SocketAddress getRemoteSocketAddress()
+    {
+
+        return socket.getRemoteSocketAddress();
+    }
+
+
+    public void close() throws IOException
+    {
+
+        in.close();
+        out.close();
+        socket.close();
+    }
 }
+
