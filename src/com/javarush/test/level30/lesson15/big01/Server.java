f8a23249aa955a23062146aaa358b28242dd8b38
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-02 15:52:47 +0200

    level30.lesson15.big01. Task №7  completed.

--- src/com/javarush/test/level30/lesson15/big01/Server.java
+++ src/com/javarush/test/level30/lesson15/big01/Server.java
@@ -3,12 +3,16 @@ package com.javarush.test.level30.lesson15.big01;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by Dmitry on 29.02.2016.
  */
 public class Server
 {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) throws IOException
     {
         int port = ConsoleHelper.readInt();
@@ -34,13 +38,33 @@ public class Server
 
     }
 
+    public static void sendBroadcastMessage(Message message)
+    {
+        try
+        {
+            for (Map.Entry<String, Connection> connection : connectionMap.entrySet())
+            {
+                Connection conn = connection.getValue();
+                conn.send(message);
+
+            }
+        }
+        catch (IOException e)
+        {
+            ConsoleHelper.writeMessage("error");
+        }
+    }
+
+
     private static class Handler extends Thread
     {
 
         private Socket socket;
+
         Handler(Socket socket)
         {
             this.socket = socket;
         }
     }
+
 }
