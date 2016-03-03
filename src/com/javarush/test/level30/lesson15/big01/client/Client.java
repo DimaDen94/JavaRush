82ada562f5034ed5c7f2c472b1518ea421cf2397
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-03 16:09:33 +0200

    level30.lesson15.big01. Task №13  completed.

--- src/com/javarush/test/level30/lesson15/big01/client/Client.java
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.test.level30.lesson15.big01.client;
 
 import com.javarush.test.level30.lesson15.big01.Connection;
+import com.javarush.test.level30.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level30.lesson15.big01.Message;
+import com.javarush.test.level30.lesson15.big01.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by Dmitry on 03.03.2016.
@@ -9,6 +14,41 @@ public class Client
 {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("input address");
+        return ConsoleHelper.readString();
+    }
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("input server port");
+        return ConsoleHelper.readInt();
+    }
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("input server port");
+        return ConsoleHelper.readString();
+    }
+    protected boolean shouldSentTextFromConsole(){
+        return true;
+    }
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+    protected void sendTextMessage(String text){
+        Message message = new Message(MessageType.TEXT, text);
+        try
+        {
+            connection.send(message);
+        }
+        catch (IOException e)
+        {
+            ConsoleHelper.writeMessage("error");
+            clientConnected = false;
+        }
+    }
+
+
+
+
     public class SocketThread extends Thread{
 
     }
