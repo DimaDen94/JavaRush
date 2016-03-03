5c9ccbb75cae5211dc5c53792afe1df898898d24
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-04 00:19:31 +0200

    level30.lesson15.big01. Task №16  completed.

--- src/com/javarush/test/level30/lesson15/big01/client/Client.java
+++ src/com/javarush/test/level30/lesson15/big01/client/Client.java
@@ -117,6 +117,38 @@ public class Client
         }
 
 
+        protected void clientHandshake() throws IOException,ClassNotFoundException
+        {
+            while (true){
+                Message message = connection.receive();
+                if(message.getType()==MessageType.NAME_REQUEST){
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+                }else if(message.getType()==MessageType.NAME_ACCEPTED){
+                    notifyConnectionStatusChanged(true);
+                    return;
+                }else
+                    throw new IOException("Unexpected MessageType");
+            }
+        }
+        protected void clientMainLoop() throws IOException, ClassNotFoundException{
+            while (true)
+            {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT)
+                {
+                    processIncomingMessage(message.getData());
+                } else if (message.getType() == MessageType.USER_ADDED)
+                {
+                    informAboutAddingNewUser(message.getData());
+                } else if (message.getType() == MessageType.USER_REMOVED)
+                {
+                    informAboutDeletingNewUser(message.getData());
+                } else
+                    throw new IOException("Unexpected MessageType");
+            }
+        }
+
+
         protected void processIncomingMessage(String message)
         {
             ConsoleHelper.writeMessage(message);
