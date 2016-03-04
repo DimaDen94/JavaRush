53b2a9d8bddeb74beeabf7c38e8adffda6af3fe2
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-04 17:50:51 +0200

    level30.lesson15.big01. Task №19  completed.

--- src/com/javarush/test/level30/lesson15/big01/client/BotClient.java
+++ src/com/javarush/test/level30/lesson15/big01/client/BotClient.java
@@ -1,5 +1,10 @@
 package com.javarush.test.level30.lesson15.big01.client;
 
+import com.javarush.test.level30.lesson15.big01.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Random;
 
 /**
@@ -12,6 +17,7 @@ public class BotClient extends Client
         BotClient bot = new BotClient();
         bot.run();
     }
+
     @Override
     protected SocketThread getSocketThread()
     {
@@ -28,10 +34,67 @@ public class BotClient extends Client
     protected String getUserName()
     {
 
-        return "date_bot_" + (new Random().nextInt() * 99);
+        return "date_bot_" + (new Random().nextInt(99));
     }
 
-    public class BotSocketThread extends SocketThread{
+    public class BotSocketThread extends SocketThread
+    {
 
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException
+        {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
+
+        @Override
+        protected void processIncomingMessage(String message)
+        {
+            ConsoleHelper.writeMessage(message);
+            String name = "";
+            String text;
+
+            if (message.contains(": ")) {
+                name = message.substring(0, message.indexOf(": "));
+                text = message.substring(message.indexOf(": ") + 2);
+            }
+            else {
+                text = message;
+            }
+
+            SimpleDateFormat format = null;
+            switch (text)
+            {
+                case "дата":
+                    format = new SimpleDateFormat("d.MM.YYYY");
+                    break;
+                case "день":
+                    format = new SimpleDateFormat("d");
+                    break;
+                case "месяц":
+                    format = new SimpleDateFormat("MMMM");
+                    break;
+                case "год":
+                    format = new SimpleDateFormat("YYYY");
+                    break;
+                case "время":
+                    format = new SimpleDateFormat("H:mm:ss");
+                    break;
+                case "час":
+                    format = new SimpleDateFormat("H");
+                    break;
+                case "минуты":
+                    format = new SimpleDateFormat("m");
+                    break;
+                case "секунды":
+                    format = new SimpleDateFormat("s");
+                    break;
+
+            }
+            if (format != null)
+            {
+                sendTextMessage("Информация для " + name + ": " + format.format(Calendar.getInstance().getTime()));
+            }
+        }
     }
 }
