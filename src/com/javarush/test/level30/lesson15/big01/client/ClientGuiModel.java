9c2d27ff76304403c24c2cf2d92637be8397e2d9
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-04 18:04:52 +0200

    level30.lesson15.big01. Task №20  completed.

---
+++ src/com/javarush/test/level30/lesson15/big01/client/ClientGuiModel.java
@@ -0,0 +1,38 @@
+package com.javarush.test.level30.lesson15.big01.client;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Dmitry on 04.03.2016.
+ */
+public class ClientGuiModel
+{
+    private final Set<String> allUserNames =  new HashSet<>();
+
+    private String newMessage;
+
+
+    public void addUser(String newUserName){
+        allUserNames.add(newUserName);
+    }
+    public void deleteUser(String userName){
+        allUserNames.remove(userName);
+    }
+
+    public String getNewMessage()
+    {
+        return newMessage;
+    }
+
+    public void setNewMessage(String newMessage)
+    {
+        this.newMessage = newMessage;
+    }
+
+    public Set<String> getAllUserNames()
+    {
+        return Collections.unmodifiableSet(allUserNames);
+    }
+}
