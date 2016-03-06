9071bdf57eb4667fcf12c774696c327ee0328b55
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-06 17:06:40 +0200

    level31.lesson15.big01  task №2 completed.

--- src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.test.level31.lesson15.big01;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by Dmitry on 06.03.2016.
@@ -14,6 +18,24 @@ public class ZipFileManager
         this.zipFile = zipFile;
     }
 
-    public void createZip(Path source) throws Exception{}
+    public void createZip(Path source) throws Exception
+    {
+        try (
+                ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+                InputStream inputStream = Files.newInputStream(source)
+        )
+        {
+            ZipEntry entry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(entry);
+            byte[] buf = new byte[1024];
+            while (inputStream.available() > 0)
+            {
+                int size = inputStream.read(buf);
+                zipOutputStream.write(buf, 0, size);
+            }
+            zipOutputStream.closeEntry();
+        }
+
+    }
 
 }
