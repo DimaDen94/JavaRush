d6204dee82451de2a6e6e833bce47a3b2fd1e8ad
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-06 19:26:21 +0200

    level31.lesson15.big01  task №7 completed.

--- src/com/javarush/test/level31/lesson15/big01/CommandExecutor.java
+++ src/com/javarush/test/level31/lesson15/big01/CommandExecutor.java
@@ -5,27 +5,22 @@ import com.javarush.test.level31.lesson15.big01.command.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by Dmitry on 06.03.2016.
- */
-public class CommandExecutor
-{
+public class CommandExecutor {
     private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
-    static {
 
+    static {
         allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
-        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
         allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
         allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
         allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
         allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
     }
-    private CommandExecutor()
-    {
+
+    private CommandExecutor() {
     }
 
-    public static void execute(Operation operation) throws Exception
-    {
+    public static void execute(Operation operation) throws Exception {
         allKnownCommandsMap.get(operation).execute();
     }
 }
---
+++ src/com/javarush/test/level31/lesson15/big01/exception/WrongZipFileException.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level31.lesson15.big01.exception;
+
+/**
+ * Created by Dmitry on 06.03.2016.
+ */
+public class WrongZipFileException extends Exception
+{
+}
