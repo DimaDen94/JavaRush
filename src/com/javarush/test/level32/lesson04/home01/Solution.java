517711d2cb0ef1131d161de9a4be96e31068fa76
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-10 06:11:37 +0200

    level32.lesson04.home01  completed.

--- src/com/javarush/test/level32/lesson04/home01/Solution.java
+++ src/com/javarush/test/level32/lesson04/home01/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.test.level32.lesson04.home01;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringWriter;
+import java.io.*;
 
 /* Читаем из потока
 Реализуйте логику метода getAllDataFromInputStream. Он должен вернуть StringWriter, содержащий все данные из переданного потока.
@@ -12,16 +9,23 @@ import java.io.StringWriter;
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        //StringWriter writer = getAllDataFromInputStream(new FileInputStream("testFile.log"));
+        StringWriter writer = getAllDataFromInputStream(new FileInputStream("D:\\HOBBY\\Java\\JavaRushHomeWork\\JavaRushHomeWork\\src\\com\\javarush\\test\\level32\\lesson04\\home01\\file.txt"));
         System.out.println(writer.toString());
     }
 
-    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException
-    {
-        StringWriter writer = new StringWriter(is.read());
-        writer.flush();
 
+    public static StringWriter getAllDataFromInputStream(InputStream is) throws IOException {
+        StringWriter writer = new StringWriter();
+        if (is != null)
+        {
+            InputStreamReader reader = new InputStreamReader(is);
+            int lon;
+            char[] buf = new char[10];
+            while ((lon=reader.read(buf))>0){
+                writer.write(buf,0,lon);
+            }
+        }
         return writer;
-
     }
 }
