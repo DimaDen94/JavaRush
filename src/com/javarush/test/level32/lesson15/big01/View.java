8f455a65461154be1b1e8a63079d39d46d032f21
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-11 22:05:47 +0200

    level32.lesson15.big01 task №3 completed.

--- src/com/javarush/test/level32/lesson15/big01/View.java
+++ src/com/javarush/test/level32/lesson15/big01/View.java
@@ -10,6 +10,9 @@ import java.awt.event.ActionListener;
 public class View extends JFrame implements ActionListener
 {
     private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
     @Override
     public void actionPerformed(ActionEvent e)
     {
---
+++ src/com/javarush/test/level32/lesson15/big01/listeners/FrameListener.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level32.lesson15.big01.listeners;
+
+import com.javarush.test.level32.lesson15.big01.View;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by Dmitry on 11.03.2016.
+ */
+public class FrameListener extends WindowAdapter
+{
+    private View view;
+
+    public FrameListener(View view)
+    {
+        this.view = view;
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e)
+    {
+        view.exit();
+    }
+}
