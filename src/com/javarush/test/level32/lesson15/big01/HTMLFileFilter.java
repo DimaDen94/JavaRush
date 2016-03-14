e742af935e0a9043838da2470cd3019c697f9bf3
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-14 05:01:13 +0200

    level32.lesson15.big01 task №21 completed.

---
+++ src/com/javarush/test/level32/lesson15/big01/HTMLFileFilter.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level32.lesson15.big01;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+/**
+ * Created by Dmitry on 14.03.2016.
+ */
+public class HTMLFileFilter extends FileFilter
+{
+    @Override
+    public boolean accept(File f)
+    {
+        if (f.isDirectory()) {
+            return true;
+        }
+        String fileName = f.getName().toLowerCase();
+        return fileName.endsWith(".html") || fileName.endsWith(".htm");
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "HTML и HTM файлы";
+    }
+}
