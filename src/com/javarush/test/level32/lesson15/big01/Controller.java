18cea9f36db245f01a945be72072a792c30ba4ee
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-14 03:47:56 +0200

    level32.lesson15.big01 task №15 completed.

--- src/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level32.lesson15.big01;
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 
 /**
@@ -22,19 +23,33 @@ public class Controller
         view.init();
         controller.init();
     }
+
     public Controller(View view)
     {
         this.view = view;
     }
 
-    public void init(){
+    public void init()
+    {
 
     }
-    public void exit(){
+
+    public void exit()
+    {
         System.exit(0);
     }
+
     public HTMLDocument getDocument()
     {
         return document;
+    }
+
+    public void resetDocument()
+    {
+        if(document!=null)
+            document.removeUndoableEditListener(view.getUndoListener());
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
     }
 }
