c825ff2705ab6cd33d8befe129d8b4c2f232e244
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-15 20:32:07 +0200

    level33.lesson15.big01 task№1 Completed.

---
+++ src/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level33.lesson15.big01;
+
+/**
+ * Created by Dmitry on 15.03.2016.
+ */
+public class Shortener
+{
+    public Long getId(String string)
+    {
+        return null;
+    }
+    public String getString(Long id){
+        return null;
+    }
+}
