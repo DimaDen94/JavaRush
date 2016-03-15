94329acf6d7ae24a16a82654555c9d8ed05e4875
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-15 20:51:44 +0200

    level33.lesson15.big01 task№3 Completed.

--- src/com/javarush/test/level33/lesson15/big01/Shortener.java
+++ src/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -1,15 +1,39 @@
 package com.javarush.test.level33.lesson15.big01;
 
+import com.javarush.test.level33.lesson15.big01.strategies.StorageStrategy;
+
 /**
  * Created by Dmitry on 15.03.2016.
  */
 public class Shortener
 {
-    public Long getId(String string)
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy)
     {
-        return null;
+        this.storageStrategy = storageStrategy;
     }
-    public String getString(Long id){
-        return null;
+
+    public synchronized Long getId(String string)
+    {
+
+        if (storageStrategy.containsValue(string)) {
+            return storageStrategy.getKey(string);
+        }
+        else {
+            lastId ++;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
+
+
+    }
+
+    public synchronized String getString(Long id)
+    {
+
+        return storageStrategy.getValue(id);
+
     }
 }
