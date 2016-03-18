9f30602983c4e34b70df2d70a92502a37d93106a
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-18 05:03:09 +0200

    level34.lesson15.big01 task16 Completed. my version.

--- src/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
+++ src/com/javarush/test/level34/lesson15/big01/model/LevelLoader.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level34.lesson15.big01.model;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.Set;
@@ -18,25 +22,100 @@ public class LevelLoader
 
     public GameObjects getLevel(int level)
     {
+        if (level > 60)
+        {
+            getLevel(level - 60);
+        }
+
         Set<Wall> walls = new HashSet<>();
-        Wall wall1 = new Wall(40,40);
-        Wall wall2 = new Wall(60,40);
-        Wall wall3 = new Wall(80,40);
-        walls.add(wall1);
-        walls.add(wall2);
-        walls.add(wall3);
+        Wall wall;
 
         Set<Box> boxes = new HashSet<>();
-        Box box = new Box(80,80);
-        boxes.add(box);
+        Box box;
 
         Set<Home> homes = new HashSet<>();
-        Home home = new Home(60,90);
-        homes.add(home);
+        Home home;
 
-        Player player = new Player(100,100);
+        Player player = null;
+
+        int y;
+
+        int x0;
+        int y0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(levels.toFile())))
+        {
+            String line;
+            while (true)
+            {
+                line = reader.readLine();
+                if (line.equals("Maze: " + level))
+                {
+                    //File offset: 148C, DS:00FC, table offset: 0000
+                    reader.readLine();
+
+                    //Size X: 22
+                    String forX = reader.readLine();
 
 
-        return new GameObjects(walls,boxes,homes,player);
+                    //Size Y: 11
+                    String forY = reader.readLine();
+                    y = Integer.parseInt(forY.replaceAll("Size Y: ", ""));
+
+                    //End: 14BD
+                    reader.readLine();
+
+                    //Length: 50
+                    reader.readLine();
+
+                    //
+                    reader.readLine();
+                    y0 = Model.FIELD_SELL_SIZE / 2;
+                    for (int ver = 0; ver < y; ver++)
+                    {
+                        line = reader.readLine();
+
+                        char[] chars = line.toCharArray();
+
+                        y0 = y0 + Model.FIELD_SELL_SIZE;
+                        x0 = Model.FIELD_SELL_SIZE / 2;
+                        for (char c : chars)
+                        {
+                            x0 = x0 + Model.FIELD_SELL_SIZE;
+                            switch (c)
+                            {
+                                case 'X':
+                                    wall = new Wall(x0, y0);
+                                    walls.add(wall);
+                                    break;
+                                case '*':
+                                    box = new Box(x0, y0);
+                                    boxes.add(box);
+                                    break;
+                                case '.':
+                                    home = new Home(x0, y0);
+                                    homes.add(home);
+                                    break;
+                                case '@':
+                                    player = new Player(x0, y0);
+                                    break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+        return new GameObjects(walls, boxes, homes, player);
     }
 }
