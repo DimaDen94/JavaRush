aac4668979556a357ac0ec7acca31bb3796bb23a
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-19 06:47:23 +0200

    level18.lesson10.bonus02 Completed.

--- src/com/javarush/test/level18/lesson10/bonus02/Solution.java
+++ src/com/javarush/test/level18/lesson10/bonus02/Solution.java
@@ -10,7 +10,8 @@ CrUD для таблицы внутри файла
 productName - название товара, 30 chars (60 bytes)
 price - цена, 8 символов
 quantity - количество, 4 символа
--c  - добавляет товар с заданными параметрами в конец файла, генерирует id самостоятельно, инкрементируя максимальный id, найденный в файле
+-c  - добавляет товар с заданными параметрами в конец файла, генерирует id самостоятельно,
+инкрементируя максимальный id, найденный в файле
 
 В файле данные хранятся в следующей последовательности (без разделяющих пробелов):
 id productName price quantity
@@ -22,7 +23,60 @@ id productName price quantity
 19847983Куртка для сноубордистов, разм10173.991234
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = bufferedReader.readLine();
+        bufferedReader.close();
+        String productName = "";
+
+        for (int i = 1; i < args.length-2; i++)
+            productName = productName + args[i] + " ";
+        String trueProductName = setLength(productName, 30);
+
+        String truePrice = setLength(args[args.length - 2], 8);
+        String trueQuantity = setLength(args[args.length - 1], 4);
+
+        String id = getId(fileName);
+        id = setLength(id, 8);
+        PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
+        printWriter.println(id + trueProductName + truePrice + trueQuantity);
+        printWriter.close();
+
+    }
+
+    public static String getId (String fileName) throws IOException {
+        ArrayList<Long> allIds = new ArrayList<Long>();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        String line;
+        Long currentId;
+        while ((line=bufferedReader.readLine()) != null) {
+            line = line.substring(0, 8).replaceAll("\\s", "");
+            currentId = Long.parseLong(line);
+            allIds.add(currentId);
+        }
+        bufferedReader.close();
+        Long maxId = Collections.max(allIds);
+        Long MyId = maxId+1;
+        return MyId.toString();
+    }
+
+
+    public static String setLength(String previousName, int count) {
+        String trueName;
+        if (previousName.length()>count)
+            trueName = previousName.substring(0, count);
+        else
+        {
+            String s="";
+            for (int i = 0; i < (count  - previousName.length()); i++)
+                s = s+ " ";
+            trueName = previousName+s;
+        }
+        return trueName;
     }
 }
