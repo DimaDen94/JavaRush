89a238c78c926370cf0f3ec28ca0c3f84b246cb5
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-29 15:43:03 +0300

    level21.lesson05.task02  Completed.

--- src/com/javarush/test/level21/lesson05/task02/Solution.java
+++ src/com/javarush/test/level21/lesson05/task02/Solution.java
@@ -7,23 +7,29 @@ import java.util.Set;
 Сравнение объектов Solution не работает должным образом. Найти ошибку и исправить.
 Метод main не участвует в тестировании.
 */
-public class Solution {
+public class Solution
+{
     private final String first, last;
 
-    public Solution(String first, String last) {
+    public Solution(String first, String last)
+    {
         this.first = first;
         this.last = last;
     }
 
-    public boolean equals(Object o) {
-        if (o==null)
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
             return false;
+        Solution solution = (Solution) o;
 
-        if (!(o instanceof Solution))
-            return false;
-        Solution n = (Solution) o;
-        return n.first.equals(first) && n.last.equals(last);
+        if (first != null ? !first.equals(solution.first) : solution.first != null) return false;
+        if (last != null ? !last.equals(solution.last) : solution.last != null) return false;
+
+        return true;
     }
+
     @Override
     public int hashCode()
     {
@@ -32,7 +38,8 @@ public class Solution {
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Set<Solution> s = new HashSet<>();
         s.add(new Solution("Mickey", "Mouse"));
         System.out.println(s.contains(new Solution("Mickey", "Mouse")));
