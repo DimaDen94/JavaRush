63b3d0dd602a77b8abcec832444421a312dca592
DimaDen94 <DimaDen94@MAIL.RU>
2016-03-31 22:12:20 +0300

    level26.lesson02.task01 Completed.

--- src/com/javarush/test/level26/lesson02/task01/Solution.java
+++ src/com/javarush/test/level26/lesson02/task01/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level26.lesson02.task01;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /* Почитать в инете про медиану выборки
@@ -9,15 +10,25 @@ import java.util.Comparator;
 */
 public class Solution {
     public static Integer[] sort(Integer[] array) {
-        Comparator<Integer> integerComparator = new Comparator<Integer>()
+        Arrays.sort(array);
+        final double median;
+        if (array.length % 2 == 0)
+            median = ((double)array[array.length/2-1]+(double)array[array.length/2])/2;
+        else
+            median = array[array.length/2];
+        Comparator<Integer> compareByMedian = new Comparator<Integer>()
         {
             @Override
             public int compare(Integer o1, Integer o2)
             {
-                return 0;
+                double value = Math.abs(o1-median) - Math.abs(o2-median);
+                if (value == 0)
+                    value = o1 - o2;
+                return (int)value;
             }
         };
-        //implement logic here
+        Arrays.sort(array, compareByMedian);
+        System.out.println(median);
         return array;
     }
 }
