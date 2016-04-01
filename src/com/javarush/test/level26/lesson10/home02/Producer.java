2afc1764090c13dac3d11a50c46b3bcdbf00ea7e
DimaDen94 <DimaDen94@MAIL.RU>
2016-04-01 12:35:49 +0300

    level26.lesson10.home02 Completed.

--- src/com/javarush/test/level26/lesson10/home02/Producer.java
+++ src/com/javarush/test/level26/lesson10/home02/Producer.java
@@ -1,8 +1,25 @@
 package com.javarush.test.level26.lesson10.home02;
 
-/**
- * Created by Dmitry on 15.02.2016.
- */
-public class Producer
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Producer implements Runnable
 {
+    protected ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    public void run() {
+        try {
+            int i = 1;
+            while (true) {
+                System.out.println("Some text for "+i++);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+    }
+
 }
--- src/com/javarush/test/level26/lesson10/home02/Solution.java
+++ src/com/javarush/test/level26/lesson10/home02/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level26.lesson10.home02;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /* Мир не меняется, меняемся мы.
 Разберитесь с ConcurrentHashMap
 В отдельном файле создайте класс Producer, который будет:
@@ -7,5 +11,19 @@ package com.javarush.test.level26.lesson10.home02;
 2. при возникновении исключения выводить в консоль [[TREAD_NAME] thread was terminated], пример "[thread-1] thread was terminated"
 */
 public class Solution {
+    public static void main(String[] args) throws Exception {
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
 
+        Producer producer = new Producer(map);
+        Consumer consumer = new Consumer(map);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(producer);
+        executorService.submit(consumer);
+
+        Thread.sleep(2000);
+
+        executorService.shutdownNow();
+        //finally 5 lines have to be printed
+    }
 }
