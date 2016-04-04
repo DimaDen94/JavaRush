ee83d77eec159a65d509945cd2521a8c3602ae41
DimaDen94 <DimaDen94@MAIL.RU>
2016-04-04 16:42:53 +0300

    level32.lesson08.bonus01Completed.

---
+++ src/com/javarush/test/level32/lesson08/bonus01/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level32.lesson08.bonus01;
+
+import java.lang.reflect.Proxy;
+
+/* Дженерики для создания прокси-объекта
+В классе Solution создайте публичный метод getProxy
+1) Метод getProxy должен возвращать прокси для любого интерфейса, который наследуется от Item
+2) getProxy должен иметь два параметра. Первый - класс возвращаемого типа, второй - классы дополнительных интерфейсов.
+3) Используйте ItemInvocationHandler для создания прокси
+Метод main не участвует в тестировании
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        test(solution.getProxy(Item.class));                        //true false false
+        test(solution.getProxy(Item.class, Small.class));           //true false true
+        test(solution.getProxy(Item.class, Big.class, Small.class));//true true true
+        test(solution.getProxy(Big.class, Small.class));            //true true true т.к. Big наследуется от Item
+        test(solution.getProxy(Big.class));                         //true true false т.к. Big наследуется от Item
+    }
+    public <T extends Item> T getProxy(Class<T> clazz, Class<?>... classes) {
+        Class<?>[] interfaces = new Class[classes.length + 1];
+        System.arraycopy(classes, 0, interfaces, 0, classes.length);
+        interfaces[interfaces.length - 1] = clazz;
+        return (T) Proxy.newProxyInstance(clazz.getClassLoader(), interfaces, new ItemInvocationHandler());
+    }
+
+    private static void test(Object proxy) {
+        boolean isItem = proxy instanceof Item;
+        boolean isBig = proxy instanceof Big;
+        boolean isSmall = proxy instanceof Small;
+
+        System.out.format("%b %b %b\n", isItem, isBig, isSmall);
+    }
+}
